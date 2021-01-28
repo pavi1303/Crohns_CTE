eacedffdc7038b79538f566563e9b1ca872e8bd9
Pavithran PG <59095277+pavi1303@users.noreply.github.com>
2021-01-28 11:28:21 -0500

    Update v1
    Intiaal update - figuring out the overall structure.

--- 4.ML_model/Cross_validation/CV_model_train.m
+++ 4.ML_model/Cross_validation/CV_model_train.m
@@ -6,63 +6,66 @@ clear
 addpath(genpath('W:\CT_Rectal'))
 addpath(genpath('W:\Final_resampled_results\Featstack_subsets'))
 addpath(genpath('W:\My_code'))
-cd('W:\Final_resampled_results\AUC_results\fea_variables\wilcoxon\4.IS_ICC_pruning');
 
-load('CT_TI_featstack_3Dresampled_IS_ICCpruning_grp1.mat');
-dontuse = [71];
+dontuse = 71;
 fselmeth='wilcoxon';
 pruning=1;
-num_top_feats=10;
-
-try
-  featstack_full=featstack_TI_full;
-  featstack_half=featstack_TI_half;
-  featstack_safire4=featstack_TI_safire4;
-
-  featstack_full(isnan(featstack_TI_full))=0;
-  featstack_half(isnan(featstack_TI_half))=0;
-  featstack_safire4(isnan(featstack_TI_safire4))=0;
-catch
-  featstack_TI_full(isnan(featstack_TI_full))=0;
-  featstack_TI_half(isnan(featstack_TI_half))=0;
-  featstack_TI_safire4(isnan(featstack_TI_safire4))=0;
-end
-
- featstack_full(isnan(featstack_full))=0;
- featstack_half(isnan(featstack_half))=0;
-featstack_safire4(isnan(featstack_safire4))=0;
-
-load('CTsubsets_lmn3.mat')%replace this with the needed subset
-
-list_dat=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90]';
-newlist=setdiff(list_dat,dontuse);
-HO_AUC={};
-fea_cell={};
-for gset=1:1
-    clear alpha* beta*
-    switch gset
-        case 1
-
-             labels = labels(:,newlist);
-            
-             trainingdata=featstack_full(newlist,:);
-             testingdata1=featstack_full(newlist,:);
-             testingdata2=featstack_half(newlist,:);
-             testingdata3=featstack_safire4(newlist,:);
-
-        case 2
-
-            trainingdata=featstack_half;
-            testingdata2=featstack_safire4;
+num_top_feats=6;
+pruning_stage = ["NO", "ICC", "IS", "IS_ICC"];
+for pset = 1:4
+    pruning_level = pruning_stage(1,pset);
+    switch pruning_level
+        case 'NO'
+            load('CT_TI_featstack_3Dresampled_nopruning_grp1.mat');
+            cd('W:\Final_resampled_results\AUC_results\Reformatted\Cross_training\1.No_pruning');
+        case 'ICC'
+            load('CT_TI_featstack_3Dresampled_ICCpruning_grp1.mat');
+            cd('W:\Final_resampled_results\AUC_results\Reformatted\Cross_training\2.ICC_pruning');
+        case 'IS'
+            load('CT_TI_featstack_3Dresampled_ISpruning_grp1.mat');
+            cd('W:\Final_resampled_results\AUC_results\Reformatted\Cross_training\3.IS_pruning');
+        case 'IS_ICC'
+            load('CT_TI_featstack_3Dresampled_IS_ICCpruning_grp1.mat');
+            cd('W:\Final_resampled_results\AUC_results\Reformatted\Cross_training\IS_ICC_pruning');
     end
+    %-----NaN check---------------------%
+    HO_AUC={};
+    fea_cell={};
     HO_AUC1=[];
     HO_AUC2=[];
     HO_AUC3=[];
-    fea_store={};
-    tic
+    try
+        featstack_full=featstack_TI_full;
+        featstack_half=featstack_TI_half;
+        featstack_safire4=featstack_TI_safire4;
+        
+        featstack_full(isnan(featstack_TI_full))=0;
+        featstack_half(isnan(featstack_TI_half))=0;
+        featstack_safire4(isnan(featstack_TI_safire4))=0;
+    catch
+        featstack_TI_full(isnan(featstack_TI_full))=0;
+        featstack_TI_half(isnan(featstack_TI_half))=0;
+        featstack_TI_safire4(isnan(featstack_TI_safire4))=0;
+    end
+    
+    featstack_full(isnan(featstack_full))=0;
+    featstack_half(isnan(featstack_half))=0;
+    featstack_safire4(isnan(featstack_safire4))=0;
+    
+    load('CTsubsets_lmn3.mat')%subset
+    pat_id = (1:90)';
+    newlist=setdiff(pat_id,dontuse);
+    
+    labels = labels(:,newlist);
+    
+    trainingdata=featstack_full(newlist,:);
+    testingdata1=featstack_full(newlist,:);
+    testingdata2=featstack_half(newlist,:);
+    testingdata3=featstack_safire4(newlist,:);
+    
     for iter=1:100
-         trainingsplit=subsets(iter).training;
-         testingsplit=subsets(iter).testing;
+        trainingsplit=subsets(iter).training;
+        testingsplit=subsets(iter).testing;
         for fset=1:3
             fea=[];
             training_set=trainingdata(trainingsplit{fset},:);
@@ -72,18 +75,18 @@ for gset=1:1
             
             training_labels=labels(trainingsplit{fset});
             testing_labels=labels(testingsplit{fset});
-             
-             options = {'threshmeth','euclidean'};
-             foldlabels{fset} = testing_labels;
-
-               [training_set_swno,mean_val,mad_val]=sw_outlier_compensation(training_set);
-               training_set=simplewhiten(training_set,mean_val,mad_val);
-               testing_set1=simplewhiten(testing_set1,mean_val,mad_val);
-               testing_set2=simplewhiten(testing_set2,mean_val,mad_val);
-               testing_set3=simplewhiten(testing_set3,mean_val,mad_val);
-
+            
+            options = {'threshmeth','euclidean'};
+            foldlabels{fset} = testing_labels;
+            
+            [training_set_swno,mean_val,mad_val]=sw_outlier_compensation(training_set);
+            training_set=simplewhiten(training_set,mean_val,mad_val);
+            testing_set1=simplewhiten(testing_set1,mean_val,mad_val);
+            testing_set2=simplewhiten(testing_set2,mean_val,mad_val);
+            testing_set3=simplewhiten(testing_set3,mean_val,mad_val);
+            
             if pruning==0
-               set_candiF=(1:length(training_set));
+                set_candiF=(1:length(training_set));
             else
                 set_candiF=pick_best_uncorrelated_features(training_set,training_labels);
             end
@@ -98,7 +101,7 @@ for gset=1:1
                 
             else
                 
-               [IDX, ~] = rankfeatures(training_set(:,set_candiF)',training_labels,'criterion','wilcoxon','CCWeighting', 1);
+                [IDX, ~] = rankfeatures(training_set(:,set_candiF)',training_labels,'criterion','wilcoxon','CCWeighting', 1);
                 if length(find(IDX==1))>1 && size(data_set,2)>20 %occurs if CCWeighting is too high and not many features to choose from or if too few (<20) features in data_set
                     warning('Too many correlated features found. Only %i features chosen. Reduce CCWeighting in call to rankfeatures() to correct.',length(IDX)-length(find(IDX==1)));
                     fea = set_candiF(IDX(1:length(IDX)-length(find(IDX==1))));
@@ -106,11 +109,10 @@ for gset=1:1
                     fea = set_candiF(IDX(1:num_top_feats));
                 end
             end
-            iter;
-            fset;
-            [temp_stats1_sw,~] = Classify_wrapper('QDA', training_set(:,fea) , testing_set1(:,fea), training_labels(:), testing_labels(:), options);
-            [temp_stats2_sw,~] = Classify_wrapper('QDA', training_set(:,fea) , testing_set2(:,fea), training_labels(:), testing_labels(:), options);
-            [temp_stats3_sw,~] = Classify_wrapper('QDA', training_set(:,fea) , testing_set3(:,fea), training_labels(:), testing_labels(:), options);
+            
+            [temp_stats1_sw,~] = Classify_wrapper('RANDOMFOREST', training_set(:,fea) , testing_set1(:,fea), training_labels(:), testing_labels(:), options);
+            [temp_stats2_sw,~] = Classify_wrapper('RANDOMFOREST', training_set(:,fea) , testing_set2(:,fea), training_labels(:), testing_labels(:), options);
+            [temp_stats3_sw,~] = Classify_wrapper('RANDOMFOREST', training_set(:,fea) , testing_set3(:,fea), training_labels(:), testing_labels(:), options);
             
             stats1(iter,fset)=temp_stats1_sw;
             stats2(iter,fset)=temp_stats2_sw;
@@ -118,7 +120,7 @@ for gset=1:1
             
             foldpredictions1_sw{fset} =  temp_stats1_sw.prediction;
             foldpredictions2_sw{fset} =  temp_stats2_sw.prediction;
-            foldpredictions3_sw{fset} =  temp_stats3_sw.prediction;  
+            foldpredictions3_sw{fset} =  temp_stats3_sw.prediction;
             
             HO_AUC1(iter,fset)=temp_stats1_sw.AUC;
             HO_AUC2(iter,fset)=temp_stats2_sw.AUC;
@@ -131,29 +133,24 @@ for gset=1:1
         [FPR,TPR,~,AUC1_sw(iter,:),OPTROCPT] = perfcurve(foldlabels, foldpredictions1_sw, 1,'XVals', [0:0.02:1]);
         [FPR,TPR,T,AUC2_sw(iter,:),OPTROCPT] = perfcurve(foldlabels, foldpredictions2_sw, 1,'XVals', [0:0.02:1]);
         [FPR,TPR,T,AUC3_sw(iter,:),OPTROCPT] = perfcurve(foldlabels, foldpredictions3_sw, 1,'XVals', [0:0.02:1]);
-        
     end
-    runtime = toc;
-
     featlist=fea_store(:,1:3);
     featlist=cell2mat(featlist);
     beta(:,1)=unique(featlist);
     beta(:,2)=count(featlist);
     beta=sortrows(beta,2,'descend');
     beta(:,2)=beta(:,2)/300;
-
+    
     HO_AUC{gset,1}=mean(mean(AUC1_sw));
     HO_AUC{gset,2}=mean(mean(AUC2_sw));
     HO_AUC{gset,3}=mean(mean(AUC3_sw));
     HO_AUC{gset,4}=mean(std(AUC1_sw));
     HO_AUC{gset,5}=mean(std(AUC2_sw));
     HO_AUC{gset,6}=mean(std(AUC3_sw));
-
-    fea_cell{gset,2}=beta;
-    clear AUC* temp* beta
-
-    temp1=fea_cell{1,2};
-    top_fea=statnames(temp1(1:num_top_feats,1));
-    top_fea=top_fea';
 end
+
+temp1=fea_cell{1,2};
+top_fea=statnames(temp1(1:num_top_feats,1));
+top_fea=top_fea';
+
 %save(['CT_TI_resampled3D_entire_vol_grp1_IS_ICC_pruning_',num2str(num_top_feats),'_',fselmeth,'_','QDA.mat'],'fea_cell','HO_AUC','top_fea','HO_AUC1','HO_AUC2','HO_AUC3','stats1','stats2','stats3');
